6be599409ec1af987c4dc29540bc649bb7046370
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-08-22 14:50:39 +0100

    added dark mode

--- src/App.js
+++ src/App.js
@@ -4,25 +4,44 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import { createContext, useState } from 'react';
+import ReactSwitch from "react-switch";
+import Contact from "./components/Contact";
+
 
 
 export const ThemeContext = createContext(null);
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [darkMode, setDarkMode] = useState('light');
 
   const toggleTheme = () => {
-    setTheme((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
+    setDarkMode((currentTheme) => (currentTheme === "light" ? "dark" : "light"));
   };
+  
+  
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme}}>
-      <div className="App" id={theme}>
-        <Navbar toggleTheme={setTheme}/>
+    <ThemeContext.Provider value={{ darkMode, toggleTheme}}>
+      <div className="App" id={darkMode}>
+        <Navbar />
+
+        <div
+          className="flex h-[80px] m-0"
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+        <button className="switch z-50">
+          <ReactSwitch className="m-0 z-50" onChange={toggleTheme} checked={darkMode === "dark"} />
+          <label> {darkMode === "light" ? "Light Mode" : "Dark Mode"}</label>
+        </button>
+        </div>
         <Home />
         <About />
         <Skills />
         <Projects />
+        <Contact />
       </div>
     </ThemeContext.Provider>
 
