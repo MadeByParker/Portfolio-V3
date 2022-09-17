34c222acb6c72904cef9ed5abf425ab0d9dbbb50
harry-parker6 <72020025+Parker06@users.noreply.github.com>
2022-09-18 00:47:14 +0100

    fixed form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
               <input data-aos="fade-up" className='bg-[#c5c5c5] p-2' type="text" placeholder='Name' name='name' />
               <input data-aos="fade-up" className='my-4 p-2 bg-[#c5c5c5]' type="email" placeholder='Email' name='email' />
               <textarea data-aos="fade-up" className='bg-[#c5c5c5] p-2' name="message" rows="10" placeholder='Message'></textarea>
-              <button data-aos="fade-up" id="projectsBtn" className='font-mono text-[#36d2f5] group border-[#36d2f5] border-2 px-4 py-3 my-8 mx-auto flex items-center hover:scale-x-10 hover:bg-[#36d2f5] hover:border-[#36d2f5] hover:text-[#1d1d1d] skewX-19deg transition-all ease-in-out duration-300 hover:text-bold'>Let's Collaborate</button>
+              <button data-aos="fade-up" id="projectsBtn" className='font-mono text-[#36d2f5] group border-[#36d2f5] border-2 px-4 py-5 my-10 mx-auto flex items-center hover:scale-x-10 hover:bg-[#36d2f5] hover:border-[#36d2f5] hover:text-[#1d1d1d] skewX-19deg transition-all ease-in-out duration-300 hover:text-bold'>Let's Collaborate</button>
           </form>
       </div>
     )
